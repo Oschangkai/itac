ba47aaaabfcd63d0bf41af9137672b44f4696139
KaiYi.C <oschangkai@gmail.com>
2016-07-09 23:54:56 +0800

    init nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
-$(function(){
-  $.scrollIt();
+$(function() {
+  $.scrollIt({
+  	topOffset: -86
+  });
 });
-$('.section100').parallax({imageSrc: 'assets/img/home-fit.jpg'});
+
+$('.section100').parallax({ imageSrc: 'assets/img/home-fit.jpg' });
+$(window).scroll(function() {
+    if ($(document).scrollTop() >= $(window).height()/6) { 
+    	$('.navbar').addClass('act2');
+    	$('.navbar').removeClass('act1');} 
+    else { 
+    	$('.navbar').addClass('act1');
+    	$('.navbar').removeClass('act2'); } 
+    });
