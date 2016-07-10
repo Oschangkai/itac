19291e0d79b4d6ec60c48f7ba81ba05a475bc15a
KaiYi.C <oschangkai@gmail.com>
2016-07-10 18:41:15 +0800

    Nav color && mobileVer

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,18 +5,24 @@ $(function() {
   });
   //scrolling effect
   $.scrollIt({
-    topOffset: -86
+    topOffset: -90
   });
-  //Sidebar
 });
 //parallax effect
 $('.section100').parallax({ imageSrc: 'assets/img/home-fit.jpg' });
 //navbar controller
 $(window).scroll(function() {
     if ($(document).scrollTop() >= $(window).height()/6) { 
-      $('.navbar').addClass('act2');
-      $('.navbar').removeClass('act1');} 
-    else { 
-      $('.navbar').addClass('act1');
-      $('.navbar').removeClass('act2'); } 
+      $('.navbar').addClass('act2').removeClass('act1');
+    }else { 
+      $('.navbar').addClass('act1').removeClass('act2');
+    } 
     });
+//Remove elements when mobile
+$(window).resize(function() {
+  if ($(window).width() < 760){
+    $('.desktop').attr('id', 'mobile');
+  }else {
+    $('.desktop').removeAttr('id');
+  };
+});;
