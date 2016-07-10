d1ca54a69e3a33365e5b9bc0f5bc865d9a8be1d6
KaiYi.C <oschangkai@gmail.com>
2016-07-10 13:43:09 +0800

    add side bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,22 @@
 $(function() {
-	// Disable animations/transitions until the page has loaded.
-	$('body').addClass('is-loading');
-	$(document).ready(function($) {
-		$('body').removeClass('is-loading');
-	});
-  $.scrollIt({
-  	topOffset: -86
+  // Disable the page until loaded.
+  $(window).on('load', function() {
+    $('load').remove();
   });
+  //scrolling effect
+  $.scrollIt({
+    topOffset: -86
+  });
+  //Sidebar
 });
-
+//parallax effect
 $('.section100').parallax({ imageSrc: 'assets/img/home-fit.jpg' });
+//navbar controller
 $(window).scroll(function() {
     if ($(document).scrollTop() >= $(window).height()/6) { 
-    	$('.navbar').addClass('act2');
-    	$('.navbar').removeClass('act1');} 
+      $('.navbar').addClass('act2');
+      $('.navbar').removeClass('act1');} 
     else { 
-    	$('.navbar').addClass('act1');
-    	$('.navbar').removeClass('act2'); } 
+      $('.navbar').addClass('act1');
+      $('.navbar').removeClass('act2'); } 
     });
