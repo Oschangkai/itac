105fc13e16dd5bbd2218e1407c50f2c1ee7f757a
KaiYi.C <oschangkai@gmail.com>
2016-08-06 23:43:06 +0800

    init timeline && about

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,30 +8,21 @@ $(function() {
     topOffset: -85
   });
 });
-//Remove elements when mobile
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $('.desktop').attr('id', 'mobile');
-}
-$(window).resize(function() {
-  if ($(window).width() < 760){
-    $('.desktop').attr('id', 'mobile');
-  }else {
-    $('.desktop').removeAttr('id');
-  };
-});
 //parallax effect
 $('.section100').parallax({ imageSrc: 'assets/img/home-fit.jpg' });
 //navbar controller
 $(window).scroll(function() {
-  if ($(document).scrollTop() >= $(window).height()/6) { 
+  if ($(document).scrollTop() >= $(window).height()/6) {
     $('.navbar').addClass('act2').removeClass('act1');
-  }else { 
+  } else {
     $('.navbar').addClass('act1').removeClass('act2');
-  } 
+  }
 });
-// hide bar on phone
+// hide bar on phone - no functional right now
+/*
 window.onload = function(){
     setTimeout(function(){
         window.scrollTo(0, 1);
     }, 100);
 }
+*/
